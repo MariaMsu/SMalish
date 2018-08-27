e6a2dde8627014bbb1394e59d6f681983abccc41
MariaMsu <i.knaz@yandex.ru>
2018-08-27 16:16:54 +0300

    add connection with db

---
+++ src/main/java/hello/MainController.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+public class MainController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping(path="/add") // Map ONLY GET Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+//        userRepository.save(n);
+
+        jdbcTemplate.update("INSERT INTO user (id, name, email) VALUES (667,?,?)", name, email);
+        return "Saved";
+    }
+
+}
