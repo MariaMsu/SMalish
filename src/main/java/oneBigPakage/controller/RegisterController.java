f1c1ca95143b9f47536a30e5202bba8474beae41
dovvakkin <dovvakkin@gmail.com>
2018-08-28 19:09:14 +0300

    bebebebeBEBEBEBEBE

--- src/main/java/oneBigPakage/controller/RegisterController.java
+++ src/main/java/oneBigPakage/controller/RegisterController.java
@@ -7,23 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
-//@Controller    // This means that this class is a Controller
-//@RequestMapping(path="/") // This means URL's start with /demo (after entity.Application path)
-//public class MainController {
-//
-//    @Autowired
-//    JdbcTemplate jdbcTemplate;
-//
-//    @GetMapping(path="/add") // Map ONLY GET Requests
-//    public @ResponseBody String addNewUser (@RequestParam String name
-//            , @RequestParam String email) {
-//
-//        jdbcTemplate.update("INSERT INTO user (name, email) VALUES (?,?)", name, email);
-//        return "Saved";
-//    }
-//}
-
 @Controller
 @RequestMapping("/")
 public class RegisterController {
